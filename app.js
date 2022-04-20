787a4c69e7cc6dc7b0138dbd9e4bb8d0c9e90106
Steph <sweetcactusflower@gmail.com>
2022-04-19 23:13:52 -0400

    Modified code for form validation in app.js.

--- app.js
+++ app.js
@@ -1,58 +1,75 @@
-const form = document.getElementById('form');
-const email = document.getElementById('email');
-//const addErrorDiv = document.createElement('div');
-//const addSuccessDiv = document.createElement('div');
-//let message = 'Whoops! It looks like you forgot to add your email!';
+const form = document.querySelector('.signup');
+const email = document.querySelector('#email');
+const subBtn = document.getElementById('btn')
+
+// function validateEmail() {
+//   form.addEventListener('submit', e => {
+//     e.preventDefault();
+  
+//     let isEmailValid = checkEmail();
+//     let isFormValid = isEmailValid;
+  
+//     if (isFormValid) {
+      
+//     }
+  
+//   });
+
+//   subBtn.trigger("click");
+//   alert("Submit button clicked!");
+//   return true;
+// }
 
 form.addEventListener('submit', e => {
   e.preventDefault();
 
-  validateInputs();
+  let isEmailValid = checkEmail();
+  let isFormValid = isEmailValid;
+
+  if (isFormValid) {
+    
+  }
+
 });
 
-const validEmailCheck = email => {
+const validEmailCheck = (email) => {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
 
-const setError = (element, message) => {
-  const inputCntrl = element.parentElement;
-  const errorDisplay = inputCntrl.querySelector('.error');
+const isRequired = value => value === '' ? false : true;
 
-  errorDisplay.innerText = message;
-  inputCntrl.classList.add('error');
-  inputCntrl.classList.remove('success');
+const showError = (input, message) => {
+  const signup = input.parentElement;
+
+  signup.classList.remove('success');
+  signup.classList.add('error');
+
+  const error = signup.querySelector('small');
+  error.textContent = message;
 }
 
-const setSuccess = element => {
-  const inputCntrl = element.parentElement;
-  const errorDisplay = inputCntrl.querySelector('.error');
+const showSuccess = (input, message) => {
+  const signup = input.parentElement;
 
-  errorDisplay.innerText = '';
-  inputCntrl.classList.add('success');
-  inputCntrl.classList.remove('error');
+  signup.classList.remove('error');
+  signup.classList.add('success');
+
+  const error = formField.querySelector('small');
+  error.textContent = '';
 }
 
-const validateInputs = () => {
-  const emailValue = email.ariaValueMax.trim();
+const checkEmail = () => {
+  let valid = false;
+  const emailVal = email.value.trim();
 
-  if(emailValue === '') {
-    setError(error, 'Whoops! It looks like you forgot to add your email!');
-  } else if (!validEmailCheck(emailValue)) {
-    setError(error, 'Please provide a valid email address.');
+  if (!isRequired(email)) {
+    showError(emailVal, 'Whoops! It looks like you forgot to add your email!');
+  } else if (!isEmailValid(email)) {
+    showError(emailVal, 'Please provide a valid email address.')
   } else {
-    setSuccess(error);
+    showSuccess(emailVal);
+    valid = true;
   }
+  return valid;
 }
-
-
-// Submit their email address using an `input` field:
-// Receive an error message when the `form` is submitted if:
-
-// The `input` field is empty. The message for this error should say
-// *"Whoops! It looks like you forgot to add your email"*
-
-// The email address is not formatted correctly
-// (i.e. a correct email address should have this structure: `name@host.tld`).
-
-// The message for this error should say *"Please provide a valid email address"*
